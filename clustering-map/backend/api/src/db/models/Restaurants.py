638c0f609163bd6dfdd4710281d23419ea60b217
Leandro <leandrojaviercepeda@gmail.com>
2020-05-14 18:43:41 -0300

    feat(clustering-map): backend/api

---
+++ clustering-map/backend/api/src/db/models/Restaurants.py
@@ -0,0 +1,33 @@
+from ..mongo import db_clustering_map
+from bson import ObjectId
+from .data.restaurants import collection
+
+def add(restaurant):
+    return db_clustering_map.restaurants.insert_one(restaurant)
+
+def remove(restaurant):
+    return db_clustering_map.restaurants.delete_one({"_id": ObjectId(restaurant)})
+    
+def modify(dictionary):
+    for key in dictionary:
+        if key != "_id":
+            db_clustering_map.restaurants.update_one({"_id": ObjectId(dictionary["_id"])}, {'$set': {key: dictionary[key]}})
+    return
+
+def get(dictionary):
+    restaurants = []
+    if '_id' in dictionary:
+        restaurants = list(db_clustering_map.restaurants.find({"_id": ObjectId(dictionary["_id"])}))
+    else:
+        restaurants = list(db_clustering_map.restaurants.find(dictionary))
+
+    for restaurant in restaurants:
+        restaurant['_id'] = str(restaurant['_id'])
+
+    return restaurants
+
+def save():
+    return db_clustering_map.restaurants.insert_many(collection)
+
+def flush():
+    return db_clustering_map.restaurants.remove()
