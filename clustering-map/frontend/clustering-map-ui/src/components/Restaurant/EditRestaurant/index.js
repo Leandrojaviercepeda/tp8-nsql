923e0aa83597976750ad59300fbfe857d481b664
Leandro <leandrojaviercepeda@gmail.com>
2020-05-15 16:46:38 -0300

    feat(clustering-map): frontend/clustering-map-ui

---
+++ clustering-map/frontend/clustering-map-ui/src/components/Restaurant/EditRestaurant/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//***********************************************************************************************
+//************************************* React Components ****************************************
+//***********************************************************************************************
+import Header from '../../Header';
+import EditRestaurant from './EditRestaurant'
+
+export default function index() {
+    return (
+        <div>
+            <Header/>
+            <EditRestaurant/>
+        </div>
+    )
+}
